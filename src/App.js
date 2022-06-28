4cb7015658eeebe8a17fd678941a4b79a9281ddd
Stafa-Mus <mustafamohamed4014@gmail.com>
2022-06-28 10:48:58 +0300

    Get products

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import { Table, Container, Row, Col, Button } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = useCallback(async () => {
+    // clear any displayed products
+    setProducts([]);
+    // TODO: add pagination and use dynamic query parameters in query below
+    try {
+      const { data } = await axios.get(
+        `http://localhost:5003/products/products?limit=10&page=2`
+      );
+      setProducts(data.data);
+    } catch (error) {
+      // TODO: error handling
+    }
+  }, []);
+
+  useEffect(() => {
+    getProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{ marginTop: 50 }}>
+        <Row style={{marginBottom: 5}}  >
+          <Col className="d-flex justify-content-end">
+            <Button variant="primary" size="sm">
+              Add product
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Product</th>
+                  <th>List price</th>
+                  <th>Product model</th>
+                  <th>Category</th>
+                  <th>Brand</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product, index) => {
+                  const {
+                    productId,
+                    productName,
+                    listPrice,
+                    productModel,
+                    categoryName,
+                    brandName,
+                  } = product;
+                  return (
+                    <tr key={productId}>
+                      <td style={{ textAlign: "center" }}>{index + 1}</td>
+                      <td>{productName}</td>
+                      <td style={{ textAlign: "center" }}>{listPrice}</td>
+                      <td>{productModel}</td>
+                      <td>{categoryName}</td>
+                      <td>{brandName}</td>
+                      <td>
+                        <Button
+                          variant="primary"
+                          size="sm"
+                          style={{ margin: 5 }}
+                        >
+                          Edit
+                        </Button>
+                        <Button variant="danger" size="sm">
+                          Delete
+                        </Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
